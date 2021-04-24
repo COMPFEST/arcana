9410a788a719b6ffc6da17a33200a26720636f81
Mario <marioserano55@gmail.com>
2021-04-24 17:14:33 +0700

    Fixed example babel preset env problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,7 @@
+export * from './components/Button';
+
 export { default as Button } from './components/Button';
+
+export * from './components/Loader';
+
 export { default as Loader } from './components/Loader';
