a2cbcc64718325688d9e91962e1c9b6bf19293ab
Mario <marioserano55@gmail.com>
2021-04-27 17:34:48 +0700

    [GREEN] FINISH WITH JEST TWIN MACRO INTEGRATION

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,20 +1,24 @@
 const postcss = require('rollup-plugin-postcss');
+const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 const peerDepsExternal = require('rollup-plugin-peer-deps-external');
 const resolve = require('rollup-plugin-node-resolve');
 
+const twindConfig = {
+  extensions: ['.ts', '.tsx'],
+};
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
-      postcss({
-        config: {
-          path: './postcss.config.js',
-        },
-        extensions: ['.css'],
-        minimize: true,
-        inject: {
-          insertAt: 'top',
-        },
+      peerDepsExternal(),
+
+      resolve({ extensions: twindConfig.extensions }),
+      commonjs(),
+      babel({
+        extensions: twindConfig.extensions,
+        include: ['src/**/*'],
+        exclude: 'node_modules/**',
       })
     );
     return config;
