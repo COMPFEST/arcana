763b597e0a3ddc31735b14a8f876ddc59eec2a80
Mario Serano <marioserano55@gmail.com>
2021-10-06 21:22:48 +0700

    Prepare for release

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Button, Loader } from '../.';
+import { Button, Alert } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Button type="primary">Testing</Button>
-      <Loader visible />
+      <Alert type="positive">tes</Alert>
     </div>
   );
 };
